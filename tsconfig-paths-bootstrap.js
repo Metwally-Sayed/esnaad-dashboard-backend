a1bd13bb9a3d9895fad7e7aa09052662e9167620
Metwally <metwally@Metwallys-MacBook-Air.local>
2026-01-04 23:09:40 +0200

    Fix tsconfig-paths baseUrl for production

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -3,14 +3,14 @@ const path = require('path');
 const tsConfig = require('./tsconfig.json');
 const tsConfigPaths = require('tsconfig-paths');
 
-const baseUrl = path.join(__dirname, 'dist'); // Absolute path to compiled JavaScript
+const baseUrl = __dirname; // Project root
 const paths = {};
 
 // Convert TypeScript paths to JavaScript paths
 if (tsConfig.compilerOptions?.paths) {
   Object.keys(tsConfig.compilerOptions.paths).forEach(alias => {
     const originalPaths = tsConfig.compilerOptions.paths[alias];
-    paths[alias] = originalPaths.map(p => p.replace('src', 'dist'));
+    paths[alias] = originalPaths.map(p => p.replace('src/', 'dist/'));
   });
 }
 
